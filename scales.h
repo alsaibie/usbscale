6c44b4f605ae948da2b57649bcaa5c36edc38e23
alsaibie <alsaibie@gmail.com>
2017-09-13 17:49:25 -0400

    Revert to upstream but keep Cmake

--- scales.h
+++ scales.h
@@ -25,21 +25,21 @@
 //
 uint16_t scales[NSCALES][2] = {\
     // Stamps.com Model 510 5LB Scale
-    {0x1446, 0x6a73},
-    // USPS (Elane) PS311 "XM Elane Elane UParcel 30lb"
-    {0x7b7c, 0x0100},
-    // Stamps.com Stainless Steel 5 lb. Digital Scale
-    {0x2474, 0x0550},
-    // Stamps.com Stainless Steel 35 lb. Digital Scale
-    {0x2474, 0x3550},
-    // Mettler Toledo
-    {0x0eb8, 0xf000},
-    // SANFORD Dymo 10 lb USB Postal Scale
-    {0x6096, 0x0158},
-    // Fairbanks Scales SCB-R9000
-    {0x0b67, 0x555e},
-    // Dymo-CoStar Corp. M25 Digital Postal Scale
-    {0x0922, 0x8004},
-    // DYMO 1772057 Digital Postal Scale
-    {0x0922, 0x8003}
+        {0x1446, 0x6a73},
+        // USPS (Elane) PS311 "XM Elane Elane UParcel 30lb"
+        {0x7b7c, 0x0100},
+        // Stamps.com Stainless Steel 5 lb. Digital Scale
+        {0x2474, 0x0550},
+        // Stamps.com Stainless Steel 35 lb. Digital Scale
+        {0x2474, 0x3550},
+        // Mettler Toledo
+        {0x0eb8, 0xf000},
+        // SANFORD Dymo 10 lb USB Postal Scale
+        {0x6096, 0x0158},
+        // Fairbanks Scales SCB-R9000
+        {0x0b67, 0x555e},
+        // Dymo-CoStar Corp. M25 Digital Postal Scale
+        {0x0922, 0x8004},
+        // DYMO 1772057 Digital Postal Scale
+        {0x0922, 0x8003}
 };
--- usbscale.c
+++ usbscale.c
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To enable a bunch of extra debugging data, simply define `#define DEBUG`
 // here and recompile.
 //
-//     #define DEBUG
+    #define DEBUG
 //
 // The "scales.h" file contains a listing of all the Vendor and Product codes
 // that this program will try to read. To try your scale, add your scale's
@@ -71,21 +71,21 @@ uint8_t get_first_endpoint_address(libusb_device* dev);
 // Point of Sale Usage Tables*, version 1.02, by the USB Implementers' Forum.
 // The list is laid out so that the unit code returned by the scale is the
 // index of its corresponding string.
-// 
+//
 const char* UNITS[13] = {
-    "units",        // unknown unit
-    "mg",           // milligram
-    "g",            // gram
-    "kg",           // kilogram
-    "cd",           // carat
-    "taels",        // lian
-    "gr",           // grain
-    "dwt",          // pennyweight
-    "tonnes",       // metric tons
-    "tons",         // avoir ton
-    "ozt",          // troy ounce
-    "oz",           // ounce
-    "lbs"           // pound
+        "units",        // unknown unit
+        "mg",           // milligram
+        "g",            // gram
+        "kg",           // kilogram
+        "cd",           // carat
+        "taels",        // lian
+        "gr",           // grain
+        "dwt",          // pennyweight
+        "tonnes",       // metric tons
+        "tons",         // avoir ton
+        "ozt",          // troy ounce
+        "oz",           // ounce
+        "lbs"           // pound
 };
 
 //
@@ -94,163 +94,163 @@ const char* UNITS[13] = {
 //
 int main(void)
 {
-    libusb_device **devs;
-    int r; // holds return codes
-    ssize_t cnt;
-    libusb_device* dev;
-    libusb_device_handle* handle;
+  libusb_device **devs;
+  int r; // holds return codes
+  ssize_t cnt;
+  libusb_device* dev;
+  libusb_device_handle* handle;
 
-    int weigh_count = WEIGH_COUNT -1;
+  int weigh_count = WEIGH_COUNT -1;
 
-    //
-    // We first try to init libusb.
-    //
-    r = libusb_init(NULL);
-    // 
-    // If `libusb_init` errored, then we quit immediately.
-    //
-    if (r < 0)
-        return r;
+  //
+  // We first try to init libusb.
+  //
+  r = libusb_init(NULL);
+  //
+  // If `libusb_init` errored, then we quit immediately.
+  //
+  if (r < 0)
+    return r;
 
 #ifdef DEBUG
-        libusb_set_debug(NULL, 3);
+  libusb_set_debug(NULL, 3);
 #endif
 
-    //
-    // Next, we try to get a list of USB devices on this computer.
-    cnt = libusb_get_device_list(NULL, &devs);
-    if (cnt < 0)
-        return (int) cnt;
+  //
+  // Next, we try to get a list of USB devices on this computer.
+  cnt = libusb_get_device_list(NULL, &devs);
+  if (cnt < 0)
+    return (int) cnt;
 
-    //
-    // Once we have the list, we use **find_scale** to loop through and match
-    // every device against the scales.h list. **find_scale** will return the
-    // first device that matches, or 0 if none of them matched.
-    //
-    dev = find_scale(devs);
-    if(dev == 0) {
-        fprintf(stderr, "No USB scale found on this computer.\n");
-        return -1;
+  //
+  // Once we have the list, we use **find_scale** to loop through and match
+  // every device against the scales.h list. **find_scale** will return the
+  // first device that matches, or 0 if none of them matched.
+  //
+  dev = find_scale(devs);
+  if(dev == 0) {
+    fprintf(stderr, "No USB scale found on this computer.\n");
+    return -1;
+  }
+
+  //
+  // Once we have a pointer to the USB scale in question, we open it.
+  //
+  r = libusb_open(dev, &handle);
+  //
+  // Note that this requires that we have permission to access this device.
+  // If you get the "permission denied" error, check your udev rules.
+  //
+  if(r < 0) {
+    if(r == LIBUSB_ERROR_ACCESS) {
+      fprintf(stderr, "Permission denied to scale.\n");
     }
-    
-    //
-    // Once we have a pointer to the USB scale in question, we open it.
-    //
-    r = libusb_open(dev, &handle);
-    //
-    // Note that this requires that we have permission to access this device.
-    // If you get the "permission denied" error, check your udev rules.
-    //
-    if(r < 0) {
-        if(r == LIBUSB_ERROR_ACCESS) {
-            fprintf(stderr, "Permission denied to scale.\n");
-        }
-        else if(r == LIBUSB_ERROR_NO_DEVICE) {
-            fprintf(stderr, "Scale has been disconnected.\n");
-        }
-        return -1;
+    else if(r == LIBUSB_ERROR_NO_DEVICE) {
+      fprintf(stderr, "Scale has been disconnected.\n");
     }
-    //
-    // On Linux, we typically need to detach the kernel driver so that we can
-    // handle this USB device. We are a userspace tool, after all.
-    //
+    return -1;
+  }
+  //
+  // On Linux, we typically need to detach the kernel driver so that we can
+  // handle this USB device. We are a userspace tool, after all.
+  //
 #ifdef __linux__
-    libusb_detach_kernel_driver(handle, 0);
+  libusb_detach_kernel_driver(handle, 0);
 #endif
-    //
-    // Finally, we can claim the interface to this device and begin I/O.
-    //
-    libusb_claim_interface(handle, 0);
+  //
+  // Finally, we can claim the interface to this device and begin I/O.
+  //
+  libusb_claim_interface(handle, 0);
 
 
 
-    /*
-     * Try to transfer data about status
-     *
-     * http://rowsandcolumns.blogspot.com/2011/02/read-from-magtek-card-swipe-reader-in.html
-     */
-    unsigned char data[WEIGH_REPORT_SIZE];
-    int len;
-    int scale_result = -1;
-    
+  /*
+   * Try to transfer data about status
+   *
+   * http://rowsandcolumns.blogspot.com/2011/02/read-from-magtek-card-swipe-reader-in.html
+   */
+  unsigned char data[WEIGH_REPORT_SIZE];
+  int len;
+  int scale_result = -1;
+
+  //
+  // For some reason, we get old data the first time, so let's just get that
+  // out of the way now. It can't hurt to grab another packet from the scale.
+  //
+  r = libusb_interrupt_transfer(
+          handle,
+          //bmRequestType => direction: in, type: class,
+          //    recipient: interface
+          LIBUSB_ENDPOINT_IN | //LIBUSB_REQUEST_TYPE_CLASS |
+          LIBUSB_RECIPIENT_INTERFACE,
+          data,
+          WEIGH_REPORT_SIZE, // length of data
+          &len,
+          10000 //timeout => 10 sec
+  );
+  //
+  // We read data from the scale in an infinite loop, stopping when
+  // **print_scale_data** tells us that it's successfully gotten the weight
+  // from the scale, or if the scale or transmissions indicates an error.
+  //
+  for(;;) {
     //
-    // For some reason, we get old data the first time, so let's just get that
-    // out of the way now. It can't hurt to grab another packet from the scale.
+    // A `libusb_interrupt_transfer` of 6 bytes from the scale is the
+    // typical scale data packet, and the usage is laid out in *HID Point
+    // of Sale Usage Tables*, version 1.02.
     //
     r = libusb_interrupt_transfer(
-        handle,
-        //bmRequestType => direction: in, type: class,
-                //    recipient: interface
-        LIBUSB_ENDPOINT_IN | //LIBUSB_REQUEST_TYPE_CLASS |
-            LIBUSB_RECIPIENT_INTERFACE,
-        data,
-        WEIGH_REPORT_SIZE, // length of data
-        &len,
-        10000 //timeout => 10 sec
-        );
-    // 
-    // We read data from the scale in an infinite loop, stopping when
-    // **print_scale_data** tells us that it's successfully gotten the weight
-    // from the scale, or if the scale or transmissions indicates an error.
-    //
-    for(;;) {
-        //
-        // A `libusb_interrupt_transfer` of 6 bytes from the scale is the
-        // typical scale data packet, and the usage is laid out in *HID Point
-        // of Sale Usage Tables*, version 1.02.
-        //
-        r = libusb_interrupt_transfer(
             handle,
             //bmRequestType => direction: in, type: class,
-                    //    recipient: interface
+            //    recipient: interface
             get_first_endpoint_address(dev),
             data,
             WEIGH_REPORT_SIZE, // length of data
             &len,
             10000 //timeout => 10 sec
-            );
-        // 
-        // If the data transfer succeeded, then we pass along the data we
-        // received tot **print_scale_data**.
-        //
-        if(r == 0) {
+    );
+    //
+    // If the data transfer succeeded, then we pass along the data we
+    // received tot **print_scale_data**.
+    //
+    if(r == 0) {
 #ifdef DEBUG
-            int i;
+      int i;
             for(i = 0; i < WEIGH_REPORT_SIZE; i++) {
                 printf("%x\n", data[i]);
             }
 #endif
-            if (weigh_count < 1) {
-                scale_result = print_scale_data(data);
-                if(scale_result != 1)
-                   break;
-            }
-            weigh_count--;
-        }
-        else {
-            fprintf(stderr, "Error in USB transfer\n");
-            scale_result = -1;
-            break;
-        }
+      if (weigh_count < 1) {
+        scale_result = print_scale_data(data);
+        if(scale_result != 1)
+          break;
+      }
+      weigh_count--;
     }
-    
-    // 
-    // At the end, we make sure that we reattach the kernel driver that we
-    // detached earlier, close the handle to the device, free the device list
-    // that we retrieved, and exit libusb.
-    //
-#ifdef __linux__
-    libusb_attach_kernel_driver(handle, 0);
-#endif
-    libusb_close(handle);
-    libusb_free_device_list(devs, 1);
-    libusb_exit(NULL);
+    else {
+      fprintf(stderr, "Error in USB transfer\n");
+      scale_result = -1;
+      break;
+    }
+  }
 
-    // 
-    // The return code will be 0 for success or -1 for errors (see
-    // `libusb_init` above if it's neither 0 nor -1).
-    // 
-    return scale_result;
+  //
+  // At the end, we make sure that we reattach the kernel driver that we
+  // detached earlier, close the handle to the device, free the device list
+  // that we retrieved, and exit libusb.
+  //
+#ifdef __linux__
+  libusb_attach_kernel_driver(handle, 0);
+#endif
+  libusb_close(handle);
+  libusb_free_device_list(devs, 1);
+  libusb_exit(NULL);
+
+  //
+  // The return code will be 0 for success or -1 for errors (see
+  // `libusb_init` above if it's neither 0 nor -1).
+  //
+  return scale_result;
 }
 
 //
@@ -267,90 +267,90 @@ int main(void)
 //
 static int print_scale_data(unsigned char* dat) {
 
-    // 
-    // We keep around `lastStatus` so that we're not constantly printing the
-    // same status message while waiting for a weighing. If the status hasn't
-    // changed from last time, **print_scale_data** prints nothing.
-    // 
-    static uint8_t lastStatus = 0;
+  //
+  // We keep around `lastStatus` so that we're not constantly printing the
+  // same status message while waiting for a weighing. If the status hasn't
+  // changed from last time, **print_scale_data** prints nothing.
+  //
+  static uint8_t lastStatus = 0;
 
-    //
-    // Gently rip apart the scale's data packet according to *HID Point of Sale
-    // Usage Tables*.
-    //
-    uint8_t report = dat[0];
-    uint8_t status = dat[1];
-    uint8_t unit   = dat[2];
-    // Accoring to the docs, scaling applied to the data as a base ten exponent
-    int8_t  expt   = dat[3];
-    // convert to machine order at all times
-    double weight = (double) le16toh(dat[5] << 8 | dat[4]);
-    // since the expt is signed, we do not need no trickery
-    weight = weight * pow(10, expt);
+  //
+  // Gently rip apart the scale's data packet according to *HID Point of Sale
+  // Usage Tables*.
+  //
+  uint8_t report = dat[0];
+  uint8_t status = dat[1];
+  uint8_t unit   = dat[2];
+  // Accoring to the docs, scaling applied to the data as a base ten exponent
+  int8_t  expt   = dat[3];
+  // convert to machine order at all times
+  double weight = (double) le16toh(dat[5] << 8 | dat[4]);
+  // since the expt is signed, we do not need no trickery
+  weight = weight * pow(10, expt);
 
-    //
-    // The scale's first byte, its "report", is always 3.
-    //
-    if(report != 0x03 && report != 0x04) {
-        fprintf(stderr, "Error reading scale data\n");
-        return -1;
-    }
+  //
+  // The scale's first byte, its "report", is always 3.
+  //
+  if(report != 0x03 && report != 0x04) {
+    fprintf(stderr, "Error reading scale data\n");
+    return -1;
+  }
 
-    //
-    // Switch on the status byte given by the scale. Note that we make a
-    // distinction between statuses that we simply wait on, and statuses that
-    // cause us to stop (`return -1`).
-    //
-    switch(status) {
-        case 0x01:
-            fprintf(stderr, "Scale reports Fault\n");
-            return -1;
-        case 0x02:
-            if(status != lastStatus)
-                fprintf(stderr, "Scale is zero'd...\n");
-            break;
-        case 0x03:
-            if(status != lastStatus)
-                fprintf(stderr, "Weighing...\n");
-            break;
-        //
-        // 0x04 is the only final, successful status, and it indicates that we
-        // have a finalized weight ready to print. Here is where we make use of
-        // the `UNITS` lookup table for unit names.
-        //
-        case 0x04:
-            printf("%g %s\n", weight, UNITS[unit]);
-            return 0;
-        case 0x05:
-            if(status != lastStatus)
-                fprintf(stderr, "Scale reports Under Zero\n");
-            break;
-        case 0x06:
-            if(status != lastStatus)
-                fprintf(stderr, "Scale reports Over Weight\n");
-            break;
-        case 0x07:
-            if(status != lastStatus)
-                fprintf(stderr, "Scale reports Calibration Needed\n");
-            break;
-        case 0x08:
-            if(status != lastStatus)
-                fprintf(stderr, "Scale reports Re-zeroing Needed!\n");
-            break;
-        default:
-            if(status != lastStatus)
-                fprintf(stderr, "Unknown status code: %d\n", status);
-            return -1;
-    }
-    
-    lastStatus = status;
-    return 1;
+  //
+  // Switch on the status byte given by the scale. Note that we make a
+  // distinction between statuses that we simply wait on, and statuses that
+  // cause us to stop (`return -1`).
+  //
+  switch(status) {
+    case 0x01:
+      fprintf(stderr, "Scale reports Fault\n");
+      return -1;
+    case 0x02:
+      if(status != lastStatus)
+        fprintf(stderr, "Scale is zero'd...\n");
+      break;
+    case 0x03:
+      if(status != lastStatus)
+        fprintf(stderr, "Weighing...\n");
+      break;
+      //
+      // 0x04 is the only final, successful status, and it indicates that we
+      // have a finalized weight ready to print. Here is where we make use of
+      // the `UNITS` lookup table for unit names.
+      //
+    case 0x04:
+      printf("%g %s\n", weight, UNITS[unit]);
+      return 0;
+    case 0x05:
+      if(status != lastStatus)
+        fprintf(stderr, "Scale reports Under Zero\n");
+      break;
+    case 0x06:
+      if(status != lastStatus)
+        fprintf(stderr, "Scale reports Over Weight\n");
+      break;
+    case 0x07:
+      if(status != lastStatus)
+        fprintf(stderr, "Scale reports Calibration Needed\n");
+      break;
+    case 0x08:
+      if(status != lastStatus)
+        fprintf(stderr, "Scale reports Re-zeroing Needed!\n");
+      break;
+    default:
+      if(status != lastStatus)
+        fprintf(stderr, "Unknown status code: %d\n", status);
+      return -1;
+  }
+
+  lastStatus = status;
+  return 1;
 }
 
 //
 // find_scale
 // ----------
-// 
+//
 // **find_scale** takes a `libusb_device\*\*` list and loop through it,
 // matching each device's vendor and product IDs to the scales.h list. It
 // return the first matching `libusb_device\*` or 0 if no matching device is
@@ -359,30 +359,30 @@ static int print_scale_data(unsigned char* dat) {
 static libusb_device* find_scale(libusb_device **devs)
 {
 
-    int i = 0;
-    libusb_device* dev = 0;
+  int i = 0;
+  libusb_device* dev = 0;
 
-    //
-    // Loop through each USB device, and for each device, loop through the
-    // scales list to see if it's one of our listed scales.
-    //
-    while ((dev = devs[i++]) != NULL) {
-        struct libusb_device_descriptor desc;
-        int r = libusb_get_device_descriptor(dev, &desc);
-        if (r < 0) {
-            fprintf(stderr, "failed to get device descriptor");
-            return NULL;
-        }
-        int i;
-        for (i = 0; i < NSCALES; i++) {
-            if(desc.idVendor  == scales[i][0] && 
-               desc.idProduct == scales[i][1]) {
-                /*
-                 * Debugging data about found scale
-                 */
+  //
+  // Loop through each USB device, and for each device, loop through the
+  // scales list to see if it's one of our listed scales.
+  //
+  while ((dev = devs[i++]) != NULL) {
+    struct libusb_device_descriptor desc;
+    int r = libusb_get_device_descriptor(dev, &desc);
+    if (r < 0) {
+      fprintf(stderr, "failed to get device descriptor");
+      return NULL;
+    }
+    int i;
+    for (i = 0; i < NSCALES; i++) {
+      if(desc.idVendor  == scales[i][0] &&
+         desc.idProduct == scales[i][1]) {
+        /*
+         * Debugging data about found scale
+         */
 #ifdef DEBUG
 
-                fprintf(stderr,
+        fprintf(stderr,
                         "Found scale %04x:%04x (bus %d, device %d)\n",
                         desc.idVendor,
                         desc.idProduct,
@@ -410,31 +410,31 @@ static libusb_device* find_scale(libusb_device **devs)
                             "Manufacturer: %s\n", string);
                     libusb_close(hand);
 #endif
-                    return dev;
-                    
-                break;
-            }
-        }
+        return dev;
+
+        break;
+      }
     }
-    return NULL;
+  }
+  return NULL;
 }
 
 uint8_t get_first_endpoint_address(libusb_device* dev)
 {
-    // default value
-    uint8_t endpoint_address = LIBUSB_ENDPOINT_IN | LIBUSB_RECIPIENT_INTERFACE; //| LIBUSB_RECIPIENT_ENDPOINT;
+  // default value
+  uint8_t endpoint_address = LIBUSB_ENDPOINT_IN | LIBUSB_RECIPIENT_INTERFACE; //| LIBUSB_RECIPIENT_ENDPOINT;
 
-    struct libusb_config_descriptor *config;
-    int r = libusb_get_config_descriptor(dev, 0, &config);
-    if (r == 0) {
-        // assuming we have only one endpoint
-        endpoint_address = config->interface[0].altsetting[0].endpoint[0].bEndpointAddress;
-        libusb_free_config_descriptor(config);
-    }
+  struct libusb_config_descriptor *config;
+  int r = libusb_get_config_descriptor(dev, 0, &config);
+  if (r == 0) {
+    // assuming we have only one endpoint
+    endpoint_address = config->interface[0].altsetting[0].endpoint[0].bEndpointAddress;
+    libusb_free_config_descriptor(config);
+  }
 
-    #ifdef DEBUG
-    printf("bEndpointAddress 0x%02x\n", endpoint_address);
-    #endif
+#ifdef DEBUG
+  printf("bEndpointAddress 0x%02x\n", endpoint_address);
+#endif
 
-    return endpoint_address;
+  return endpoint_address;
 }
